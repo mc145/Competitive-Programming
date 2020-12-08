0f4a0ce59befdef245aa7c921b5cab79cf084a6b
Akshay Agarwal <mathgeek145@gmail.com>
2020-12-08 12:53:48 +0800

    changes

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+typedef long long ll; 
+typedef long double ld; 
+
+
+int main(){
+
+ios_base::sync_with_stdio(false);
+cin.tie(NULL); 
+// ifstream cin("Filename.in"); 
+// ofstream cout("Filename.out"); 
+char nl = '\n'; 
+char bl = ' ';
+
+int t; 
+cin >> t; 
+
+for(int case_num = 1; case_num <= t; case_num++){
+
+
+    // cout << "Case #" << case_num << ": "; 
+}
+
+}
+
